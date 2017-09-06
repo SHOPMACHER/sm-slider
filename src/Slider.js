dffecd624a75701f611ceeedd2c7d28cf13ffc54
Rico Herwig <rico@hrwg.de>
2017-09-06 15:09:53 +0200

    implements cloak

--- src/Slider.js
+++ src/Slider.js
@@ -143,6 +143,8 @@ export default class Slider {
                 _(this).isIntervalPaused = false;
             });
         }
+
+        _(this).$ref.classList.remove('cloaked');
     }
 
     /**
