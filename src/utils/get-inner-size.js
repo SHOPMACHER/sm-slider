ed62b47a8d6858052ea0c74dab48fbcc588ca25f
Rico Herwig <r.herwig@shopmacher.de>
2018-11-13 15:55:36 +0100

    Remove slider arrows from width calc

--- src/utils/get-inner-size.js
+++ src/utils/get-inner-size.js
@@ -5,14 +5,14 @@ const getInnerWidth = (
     $arrowRight: ?HTMLElement
 ) => {
     const sliderWidth = $slider.querySelector('.slides-wrapper').getBoundingClientRect().width;
-    const arrowLeftWidth = $arrowLeft && !$arrowLeft.classList.contains('inset')
-        ? $arrowLeft.getBoundingClientRect().width
-        : 0;
-    const arrowRightWidth = $arrowRight && !$arrowRight.classList.contains('inset')
-        ? $arrowRight.getBoundingClientRect().width
-        : 0;
+    // const arrowLeftWidth = $arrowLeft && !$arrowLeft.classList.contains('inset')
+    //     ? $arrowLeft.getBoundingClientRect().width
+    //     : 0;
+    // const arrowRightWidth = $arrowRight && !$arrowRight.classList.contains('inset')
+    //     ? $arrowRight.getBoundingClientRect().width
+    //     : 0;
 
-    return sliderWidth - arrowLeftWidth - arrowRightWidth;
+    return sliderWidth;
 };
 
 const getInnerHeight = (
@@ -21,14 +21,14 @@ const getInnerHeight = (
     $arrowBottom: ?HTMLElement
 ) => {
     const sliderHeight = $slider.querySelector('.slides-wrapper').getBoundingClientRect().height;
-    const arrowTopHeight = $arrowTop && !$arrowTop.classList.contains('inset')
-        ? $arrowTop.getBoundingClientRect().height
-        : 0;
-    const arrowBottomHeight = $arrowBottom && !$arrowBottom.classList.contains('inset')
-        ? $arrowBottom.getBoundingClientRect().height
-        : 0;
+    // const arrowTopHeight = $arrowTop && !$arrowTop.classList.contains('inset')
+    //     ? $arrowTop.getBoundingClientRect().height
+    //     : 0;
+    // const arrowBottomHeight = $arrowBottom && !$arrowBottom.classList.contains('inset')
+    //     ? $arrowBottom.getBoundingClientRect().height
+    //     : 0;
 
-    return sliderHeight - arrowTopHeight - arrowBottomHeight;
+    return sliderHeight;
 };
 
 export default (
