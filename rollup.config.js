6ed56cd0fa8d0ef292488b8949eddd8a17d105c2
Rico Herwig <rherwig4711@gmail.com>
2018-11-15 20:06:40 +0100

    remove svg arrow for replacement with css arrow

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import postcss from 'rollup-plugin-postcss';
 import autoprefixer from 'autoprefixer';
+import copy from 'postcss-copy-assets';
 import replace from 'rollup-plugin-replace';
 import { terser } from 'rollup-plugin-terser';
 
