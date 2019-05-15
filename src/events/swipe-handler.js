1a950e5df7685c04871334187e65beae52a753db
Loai Kahel <l.kahel@shopmacher.de>
2019-05-15 12:26:46 +0200

    handle swiping vertically on all devices

--- src/events/swipe-handler.js
+++ src/events/swipe-handler.js
@@ -30,8 +30,23 @@ export default (
     const restraint = 100; // maximum distance allowed at the same time in perpendicular direction
     let _slideTo = 0;
     let clientY = 0;
+    let touchOnSlider = false;
 
     if (!store.getState().isVertical) {
+        window.addEventListener('touchmove', event => {
+            const touch = event.changedTouches[0];
+
+            if (!touchOnSlider) {
+                return;
+            }
+
+            if (touch.clientY > clientY - 10 && touch.clientY < clientY + 10) {
+                $touchTarget.addEventListener('touchmove', handleTouchMove, { passive: true });
+            } else {
+                $touchTarget.removeEventListener('touchmove', handleTouchMove);
+            }
+        });
+
         $touchTarget.addEventListener('touchstart', (event: TouchEvent) => {
             const touch = event.changedTouches[0];
 
@@ -39,6 +54,8 @@ export default (
             threshold = (store.getState().innerSize / options.visibleSlides) / 5;
             startX = touch.pageX;
             startY = touch.pageY;
+            clientY = touch.clientY;
+            touchOnSlider = true;
 
             const translateMatch = $touchTarget.style.transform.match(/-?\d+/i);
             if (translateMatch && translateMatch[0]) {
@@ -48,24 +65,21 @@ export default (
 
         }, { passive: true });
 
-        $touchTarget.addEventListener('touchmove', (event: TouchEvent) => {
+        const handleTouchMove = (event: TouchEvent) => {
             const touch = event.changedTouches[0];
 
             distX = touch.pageX - startX;
             distY = touch.pageY - startY;
-            setTimeout(() => {
-                clientY = touch.clientY;
-            }, 10);
 
             if (translate !== null && translate !== undefined) {
                 $touchTarget.style.transform = `translateX(${translate + distX}px)`;
             }
+        };
 
-            touch.clientY < clientY ?  window.scrollTo(0, pageYOffset + 3) :  window.scrollTo(0, pageYOffset - 3);
-
-        }, { passive: true });
+        $touchTarget.addEventListener('touchmove', handleTouchMove, { passive: true });
 
         $touchTarget.addEventListener('touchend', (event: TouchEvent) => {
+            touchOnSlider = false;
             if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
                 swipeDirection = (distX < 0) ? 'left' : 'right';
             } else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) {
@@ -75,7 +89,6 @@ export default (
                 $touchTarget.style.transform = `translateX(${translate}px)`;
             }
 
-
             const slideWidth = window.getComputedStyle($touchTarget.children[0]).width.split('px')[0];
             _slideTo = Math.abs(Math.round((translate + distX) / slideWidth));
 
@@ -83,12 +96,20 @@ export default (
                 _slideTo = Math.abs(Math.round((translate + distX) / slideWidth)) - 1;
             }
 
+            if (distX > 0 && translate >= 0 && (distX + translate) > 0) {
+                _slideTo = 0;
+            }
+
             window.setTimeout(() => {
                 if (options.step > 1 && options.infinite) {
                     _slideTo = _slideTo - options.step + 1;
                 } else if (!options.infinite && _slideTo >= store.getState().totalSlides - store.getState().visibleSlides) {
                     _slideTo = store.getState().totalSlides - store.getState().visibleSlides;
-                    requestAnimationFrame(() => slideTo(store, _slideTo +2));
+                    requestAnimationFrame(() => slideTo(store, _slideTo + 2));
+                }
+
+                if (store.getState().currentSlide === 0 && store.getState().currentSlide === _slideTo) {
+                    slideTo(store, _slideTo + 1);
                 }
 
                 requestAnimationFrame(() => slideTo(store, _slideTo));
